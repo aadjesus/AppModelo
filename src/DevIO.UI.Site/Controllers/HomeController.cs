cb0929cbff8cc8d802a621f0c0c7eba47706d9ed
Alessandro Augusto <alessandro.augusto@bgmrodotec.com.br>
2020-03-10 15:09:39 -0300

    refactor: testes #0
    US #0

--- src/DevIO.UI.Site/Controllers/HomeController.cs
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,23 @@ namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly IPedidoRepository _pedidoRepository;
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // Outra forma de fazer uma injeção de dependencia. Obs: Somente para resolver problemas q o contrutor ñ pode ser alterado
+        public ActionResult Index([FromServices]IPedidoRepository pedidoRepository) 
+        {
+            var pedido = pedidoRepository.ObterPedido();
+
             return View();
         }
     }
--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.UI.Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DevIO.UI.Site
@@ -15,8 +17,22 @@ namespace DevIO.UI.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Jeito de altera a convenção de pasta "Area" para "Modulos"
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaPageViewLocationFormats.Clear();
+                options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml"); // 2
+                options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaPageViewLocationFormats.Add("/Modulos/Chared/{0}/{0}.cshtml");
+            });
+
             services
                 .AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();   // Obtem uma nova instancia do objeto quando for solicitado
+            // services.AddScoped<IPedidoRepository, PedidoRepository>();   // Reutiliza a mesma instancia do objeto durante todo o request (Recomenda para web)
+            //services.AddSingleton<IPedidoRepository, PedidoRepository>(); //
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,9 +49,13 @@ namespace DevIO.UI.Site
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapRoute(
-                    name: "areas",
-                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                // Pode ser subistituidos pelos atributos Route na controller
+                //routes.MapRoute(
+                //    name: "areas",
+                //    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapAreaRoute(name: "AreaProdutos", areaName: "Produtos", template: "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+                routes.MapAreaRoute(name: "AreaVendas", areaName: "Vendas", template: "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
             });
         }
     }
